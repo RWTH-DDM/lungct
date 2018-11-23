40421a5e51dc75988f98304675e63f3dfe2cb76c
Arne Groskurth <arne.groskurth@gears9.de>
2018-11-23 14:43:27 +0100

    Simple thresholding

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import glob
 import os
 import nibabel as nib
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Read in paths of available data
@@ -21,13 +22,28 @@ if len(image_files) == 0:
     raise Exception("No data found under " + data_base_path)
 
 
-# Display a central slice of the first data/mask-pair
-test_image = nib.load(image_files[0]).get_data()
-test_mask = nib.load(mask_files[0]).get_data()
+# Use first image
+scan = nib.load(image_files[0]).get_fdata()
+mask = nib.load(mask_files[0]).get_fdata()
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-ax1.imshow(test_image[test_image.shape[0] // 2])
+print("Dimensions: " + str(scan.shape))
+print("Min: " + str(np.amin(scan)))
+print("Max: " + str(np.amax(scan)))
+
+
+# Thresholding
+# Thresholds taken from https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0152505
+segmentation = np.copy(scan)
+segmentation[(-950 < segmentation) & (segmentation < -701)] = 1
+segmentation[segmentation != 1] = 0
+
+
+# Display result
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6))
+ax1.imshow(scan[scan.shape[0] // 2])
 ax1.set_title('Image')
-ax2.imshow(test_mask[test_mask.shape[0] // 2])
+ax2.imshow(mask[mask.shape[0] // 2])
 ax2.set_title('Mask')
+ax3.imshow(segmentation[segmentation.shape[0] // 2])
+ax3.set_title('Segmentation')
 fig.show()
