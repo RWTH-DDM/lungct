eae60b4a097797e4a132c07d7374de97d3ada2ca
Arne Groskurth <arne.groskurth@gears9.de>
2018-12-17 15:22:55 +0100

    Added argument- and return-types

--- lungct/LungCT.py
+++ lungct/LungCT.py
@@ -6,23 +6,23 @@ import segmentation
 
 class LungCT:
 
-    def __init__(self, scan_file_path):
+    def __init__(self, scan_file_path: str):
 
         self._scan = nib.load(scan_file_path)
 
         self._mask = None
 
-    def get_scan(self):
+    def get_scan(self) -> np.array:
 
         return self._scan.get_fdata()
 
-    def get_mask(self):
+    def get_mask(self) -> np.array:
 
         if self._mask is None:
             self._mask = segmentation.get_lung_mask(self.get_scan())
 
         return self._mask
 
-    def get_volume(self):
+    def get_volume(self) -> float:
 
         return np.count_nonzero(self.get_mask())
