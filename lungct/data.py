c6a5c4cb2b0e696fa0706304661d34043294a204
Arne Groskurth <arne.groskurth@gears9.de>
2018-12-17 12:30:35 +0100

    Removed more unused code

--- lungct/data.py
+++ lungct/data.py
@@ -11,11 +11,6 @@ _data_base_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..',
 def get_image_ids():
 
     image_files = glob.glob(os.path.join(_data_base_path, 'IMG_*'))
-    mask_files = [path.replace('IMG_', 'MASK_') for path in image_files]
-
-    for mask_file in mask_files:
-        if not os.path.isfile(mask_file):
-            raise Exception("Missing mask file " + mask_file)
 
     print("Images found: %d" % len(image_files))
 
