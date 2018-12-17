34f3f31e3beb9c88be11acf467f656ebd23256b8
Arne Groskurth <arne.groskurth@gears9.de>
2018-12-17 12:00:49 +0100

    Removed manual mask displayment

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 # Use first image
 scan = data.get_image('0002')
-mask = data.get_mask('0002')
 
 
 # Find lung
@@ -19,17 +18,13 @@ print("(Segmentation took %.2f seconds)" % (end - start))
 
 
 # Display result as central slice thru the scan, the given segmentation and the computed segmentation
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
 scan_data = scan.get_fdata()
 ax1.imshow(scan_data[scan_data.shape[0] // 2])
 ax1.set_title('Image')
 
-mask_data = mask.get_fdata()
-ax2.imshow(mask_data[mask_data.shape[0] // 2])
-ax2.set_title('Mask')
-
-ax3.imshow(segmentation[segmentation.shape[0] // 2])
-ax3.set_title('Segmentation')
+ax2.imshow(segmentation[segmentation.shape[0] // 2])
+ax2.set_title('Segmentation')
 
 fig.show()
