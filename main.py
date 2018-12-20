8a4104371492ba87914b104d357a6a1226b7680a
Arne Groskurth <arne.groskurth@gears9.de>
2018-12-20 16:29:59 +0100

    Add: Segmentations are now exported as nifti

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ from LungCT import LungCT
 lungct = LungCT(data.get_image_path('0002'))
 lung_with_vessels = lungct.get_lung()
 lung_without_vessels = lungct.get_lung_without_vessels()
+vessels = lungct.get_vessel()
 
-print("Lung with vessels:")
+# export segmentations
+print("Exporting segmentations...")
+lung_with_vessels.nifti_export('with_vessels.nii')
+lung_without_vessels.nifti_export('without_vessels.nii')
+vessels.nifti_export('vessels.nii')
+
+print("\nLung with vessels:")
 print("Volume: %f l" % lung_with_vessels.get_volume())
 print("PD 5%%: %f" % lung_with_vessels.get_percentile_density(5))
 print("PD 95%%: %f" % lung_with_vessels.get_percentile_density(95))
 print("Average: %f" % lung_with_vessels.get_average_density())
 print("Median: %f" % lung_with_vessels.get_median_density())
-print()
 
-print("Lung without vessels:")
+print("\nLung without vessels:")
 print("Volume: %f l" % lung_without_vessels.get_volume())
 print("PD 5%%: %f" % lung_without_vessels.get_percentile_density(5))
 print("PD 95%%: %f" % lung_without_vessels.get_percentile_density(95))
@@ -25,6 +31,7 @@ print("Median: %f" % lung_without_vessels.get_median_density())
 
 
 # Display result as central slice through the scan, the given segmentation and the computed segmentation
+print("\nDisplaying slices...")
 fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
 
 scan_data = lungct.get_scan()
